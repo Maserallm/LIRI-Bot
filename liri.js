ec354973889d287925ba8f3c5c77e5400bd4d8df
Maserall Marcelin <maserallm@knights.ucf.edu>
2019-11-23 15:09:39 -0500

    update to spotify search, fully functional

--- liri.js
+++ liri.js
@@ -1,34 +1,68 @@
 // code for required packages
 const axios = require("axios");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const Spotify = require("node-spotify-api");
 const keys = require("./keys.js");
-console.log(keys);
 
 const spotify = new Spotify(keys.spotify);
-// const omdbKey = new Omdb(keys.omdb);
+const omdbKey = new axios(keys.omdb);
 
-// const searchParam1 = process.argv[2];
-// const searchParam2 = process.argv[3];
+const commands = process.argv[2];
+const secCommand = process.argv[3];
 
-// //spotify.searchParam;
-// console.log(dotenv);
+//switch case statements
 
-// switch case statements
-// switch(searchParam1) {
-//     case:
+switch (commands) {
+  case "concert-this":
+    // code to search BIT API
+    if (secCommand) {
+      axios
+        .get(
+          "https://rest.bandsintown.com/artists/" +
+            secCommand +
+            "/events?app_id=codingbootcamp"
+        )
+        .then(response => {
+          console.log(response.venue.name);
+          console.log(`Location: ${response.venue.city}`);
+        })
+        .catch(err => {
+          if (err) {
+            console.log(err);
+          }
+        });
+    }
+    break;
+  case "spotify-this-song":
+    // code to pull song info
+    if (secCommand) {
+      spotSearch(secCommand);
+    } else {
+      spotSearch("Everything We Need");
+    }
+    break;
+  case "movie-this":
+    // pull movie info though OMDB
+    break;
+  case "do-what-it-says":
+    // takes random.txt file & runs switch commands
+    break;
+}
+/*function spotSearch(song) {
+  spotify.search({ type: "track", query: song, limit: 1 }, function(err, data) {
+    if (err) {
+      throw console.log("Error occurred: " + err);
+    } else {
+      for (let i = 0; i < data.tracks.items.length; i++) {
+        let songInfo = data.tracks.items[i];
 
-// }
-
-spotify.search({ type: "track", query: "All the Small Things" }, function(
-  err,
-  data
-) {
-  if (err) {
-    return console.log("Error occurred: " + err);
-  }
-
-  console.log(data);
-});
+        console.log(`Artists: ${songInfo.artists[0].name}`);
+        console.log(`Name of the Song: ${songInfo.name}`);
+        console.log(`Click to preview it: ${songInfo.preview_url}`);
+        console.log(`Album: ${songInfo.album.name}`);
+      }
+    }
+  });
+}*/
 
 //   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"", )
